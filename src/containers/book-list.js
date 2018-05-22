31014d7119992461835a30c47bac685486c87fbb
Marty Stepien <marty.stepien@gmail.com>
2018-05-22 09:57:00 +0100

    render list of books from reducer

---
+++ src/containers/book-list.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+class BookList extends Component {
+	renderList() {
+		return (this.props.books.map((book) => { 
+			return (
+				<li key = {book.title}>
+					{book.title}
+				</li>)}
+			)	
+		)
+	}
+	render() {
+		return ( 
+			<div>
+				<ul className="list-group col-sm-4">
+					{this.renderList()}
+				</ul>
+			</div>
+		);
+	}
+}
+
+function mapStateToProps(state) {
+	console.log(state.books)
+	return {
+		books: state.books
+	}
+};
+
+export default connect(mapStateToProps)(BookList);
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -12,5 +12,8 @@ export default function () {
 		{
 			title: 'Clean Code'
 		},
+		{
+			title: 'Cracking the coding interview'
+		},
 	];
-}
+};
