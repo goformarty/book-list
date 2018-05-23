f033c91b77dc5301cb960ed5d6438619edcfbdbc
Marty Stepien <marty.stepien@gmail.com>
2018-05-23 11:38:27 +0100

    display details of active book

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -3,15 +3,21 @@ import { connect } from 'react-redux';
 
 class BookDetail extends Component {
 	render() {
+		if (!this.props.book) {
+			return <div>Select a book</div>
+		}
 		return (
-			<div>Book Detail Container</div>
+			<div>
+				<h3>Details for:</h3>
+				<div>Title: {this.props.book.title}</div>
+				<div>Pages: {this.props.book.pages}</div>
+			</div>
 		);
 	}
 }
 
 function mapStateToProps(state) {
-	console.log('hey there', state);
-	book: state.activeBook;
+	return {book: state.activeBook};
 }
 
-export default connect(mapStateToProps)(BookDetail)
+export default connect(mapStateToProps)(BookDetail);
